449f2cddf69e7cc32d36e378f42e42727197134d
gdking137 <gdkim137@gmail.com>
2022-11-17 22:56:38 -0500

    added logout function

--- index.js
+++ index.js
@@ -92,7 +92,18 @@ app.post('/api/users/auth', auth, (req,res)=>{ //before callback function we add
   })
 })
 
+app.get('/api/users/logout', auth, (req, res) =>{
+  console.log('req.user', req.user)
+  User.findOneAndUpdate({ _id: req.user._id},
+  {token: ""},
+  (err, user) =>{
+    if(err) return res.json({ success:false, err});
+    return res.status(200).send({
+      success: true
+    })
+  })
 
+})
 
 
 app.listen(port, () => {
